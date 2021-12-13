60b70d1104b264b2485d522c0fa14ed837acc996
Anna Lotz <anna.lotz@web.de>
2021-12-13 21:54:12 +0100

    Startet L3.2

---
+++ L03_TimeComplexity/PermMissingElem.js
@@ -0,0 +1,14 @@
+"use strict";
+var PermMissingElem;
+(function (PermMissingElem) {
+    let A = [];
+    A[0] = 2;
+    A[1] = 3;
+    A[2] = 1;
+    A[3] = 5;
+    console.log(solution(A));
+    function solution(A) {
+        return 0;
+    }
+})(PermMissingElem || (PermMissingElem = {}));
+//# sourceMappingURL=PermMissingElem.js.map
---
+++ L03_TimeComplexity/PermMissingElem.ts
@@ -0,0 +1,17 @@
+namespace PermMissingElem {
+
+    let A: number[] = [];
+
+    A[0] = 2;
+    A[1] = 3;
+    A[2] = 1;
+    A[3] = 5;
+
+    console.log(solution(A));
+
+    function solution(A: number[]): number {
+
+        return 0;
+    }
+
+}
